f86b1755df89dda7a3472532596cb8e7d80e6965
Alex <alexedwa123@gmail.com>
2024-07-09 14:22:06 +0000

    Added visit statement list function

--- src/visitor.c
+++ src/visitor.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "include/visitor.h"
 
 AST_T* visitor_visit(AST_T* node){
@@ -7,8 +8,9 @@ AST_T* visitor_visit(AST_T* node){
         case ast_func_call: return visitor_visit_func_call(node); break;
         case ast_str: return visitor_visit_str(node); break;
         case ast_statement_list: return visitor_visit_statement_list(node); break;
+        case ast_end_of_operations: return node; break;
     }
-    //return("Unexpected statement type of %d \n", node->type);
+    printf("Unexpected statement type of %d \n", node->type);
     exit(1);
 
 }
@@ -30,5 +32,8 @@ AST_T* visitor_visit_str(AST_T* node){
 }
 
 AST_T* visitor_visit_statement_list(AST_T* node){
-
+    for (int i = 0; i < node->ast_statement_list_size; ++i){
+        visitor_visit(node->ast_statement_list_value[i]);
+    }
+    return init_ast(ast_end_of_operations);
 }
