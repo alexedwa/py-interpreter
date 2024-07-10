b181fe175acd6a33e2f5f0034d0c5abd5ed0c103
Alex <alexedwa123@gmail.com>
2024-07-10 07:03:05 +0000

    Added visitor struct and builtin func methods

--- src/include/visitor.h
+++ src/include/visitor.h
@@ -1,15 +1,26 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 #include "ast.h"
-AST_T* visitor_visit(AST_T* node);
+typedef struct VISITOR_STRUCT{
+    AST_T** visitor_variable_contents;
+    size_t visitor_variable_contents_size;
+}visitor_T;
 
-AST_T* visitor_visit_variable(AST_T* node);
+visitor_T* init_visitor();
 
-AST_T* visitor_visit_variable_contents(AST_T* node);
+AST_T* visitor_visit(AST_T* node, visitor_T* visitor);
 
-AST_T* visitor_visit_func_call(AST_T* node);
+AST_T* visitor_visit_variable(AST_T* node, visitor_T* visitor);
 
-AST_T* visitor_visit_str(AST_T* node);
+AST_T* visitor_visit_variable_contents(AST_T* node, visitor_T* visitor);
 
-AST_T* visitor_visit_statement_list(AST_T* node);
+AST_T* visitor_visit_func_call(AST_T* node, visitor_T* visitor);
+
+AST_T* visitor_visit_str(AST_T* node, visitor_T* visitor);
+
+AST_T* visitor_visit_statement_list(AST_T* node, visitor_T* visitor);
+
+// BUILTIN FUNCTIONS //
+
+static AST_T* builtin_func_print(AST_T** args, int arg_size, visitor_T* visitor);
 #endif
