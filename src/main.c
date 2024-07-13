d196453ea55f175eb3650fc5ee47f2083dccc653
Alex <alexedwa123@gmail.com>
2024-07-13 14:07:11 +0000

    Implemented reading files using argc and argv[]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
-#include "include/main.h"
-int main(int argc, char* argv[]){
+#include <stdio.h>
+#include <string.h>
+#include "include/lexer.h"
+#include "include/parser.h"
+#include "include/visitor.h"
+#include "include/io.h"
 
+int main(int argc, char* argv[]){
+	if (argc < 2){
+		printf("Write: ./py-interpreter.out <filename>\n");
+		exit(1);
+	}
+	else{
+		lexer_T* lexer = init_lexer(get_file_contents(argv[1]));
+		parser_T* parser = init_parser(lexer);
+		AST_T* root = parser_parse(parser);
+		visitor_T* visitor = init_visitor();
+		visitor_visit(root, visitor);
+	}
 	return 0;
 }
+
+// example_files/testfile1.py
