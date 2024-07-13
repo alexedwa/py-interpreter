89c59f0054a67e473846d6e5b4ca8486a9f6f734
Alex <alexedwa123@gmail.com>
2024-07-13 14:09:36 +0000

    Rewrote instructions for interpreting

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[]){
 	if (argc < 2){
-		printf("Write: ./py-interpreter.out <filename>\n");
+		printf("Write: ./py-interpreter.out <filename.py>\n");
 		exit(1);
 	}
 	else{
